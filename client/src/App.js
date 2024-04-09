dea39654e2d4479ffef9d780b5c65e4e97cb942b
ujwal Hiranwar <ujjwalhiranwar15@gmail.com>
2024-04-09 19:41:41 +0530

    backend setup and dependencies added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+import Navigation from './components/landing page/Navigation';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Navigation />
+   <Login />
+    </div>
+  );
+}
+
+export default App;
--- hospital-management-app/server.js
+++
@@ -1,3 +0,0 @@
-const http = require('http')
-const express = require('express')
-const app=express.Router();
