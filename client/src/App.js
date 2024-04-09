9be7d1f2842814618a511284ac7fa30f9a850328
ujwal Hiranwar <ujjwalhiranwar15@gmail.com>
2024-04-09 20:16:24 +0530

    backend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-
-
-import Navigation from './components/landing page/Navigation';
-import Login from './components/Login';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-   <Navigation />
-   <Login />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
