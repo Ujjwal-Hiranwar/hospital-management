20d2b2476f17205d56f2b6098d94752e3e56d0e7
ujwal Hiranwar <ujjwalhiranwar15@gmail.com>
2024-04-09 22:56:27 +0530

    backend folders created

--- server/app.js
+++ server/app.js
@@ -1,2 +1,13 @@
 const express = require('express')
-console.log("hii")
+const app = express()
+
+app.use(express.json())
+
+
+
+
+
+app.listen(8000,(err)=>{
+    if(err) console.log("error in listen : "+err)
+    console.log("server is listning")
+})
